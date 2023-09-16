886f0547a560db4e1b8f1b6d6a39245c9f870d5a
Ron Megini <58236438+ronmegini@users.noreply.github.com>
2023-09-16 11:25:34 +0300

    Update Trends.py

--- user_data/strategies/Trends.py
+++ user_data/strategies/Trends.py
@@ -15,22 +15,6 @@ import freqtrade.vendor.qtpylib.indicators as qtpylib
 from findiff import FinDiff
 
 class Trends(IStrategy):
-    """
-    This is a strategy template to get you started.
-    More information in https://www.freqtrade.io/en/latest/strategy-customization/
-
-    You can:
-        :return: a Dataframe with all mandatory indicators for the strategies
-    - Rename the class name (Do not forget to update class_name)
-    - Add any methods you want to build your strategy
-    - Add any lib you need to build your strategy
-
-    You must keep:
-    - the lib in the section "Do not remove these libs"
-    - the methods: populate_indicators, populate_buy_trend, populate_sell_trend
-    You should keep:
-    - timeframe, minimal_roi, stoploss, trailing_*
-    """
     INTERFACE_VERSION = 2
     timeframe = '1h'
     minimal_roi = {
